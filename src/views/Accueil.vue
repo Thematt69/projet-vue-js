fdb0ffa5ff9a5c09f5698993b8d357db06387edb
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-02-10 14:14:06 +0100

    add Bootstrap

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,25 +1,33 @@
 <template>
-  <div class="accueil">
+  <div class="accueil" v-show="store">
     <h1>{{ txt }}</h1>
     <h2>{{ config.ville }}</h2>
 
-    <p>
-      Météo : {{ meteo }}
-    </p>
-    <p>
-      Risque de précipitations : {{ risquePluie }}
-    </p>
+    <p>Météo : {{ meteo }}</p>
+    <p>Risque de précipitations : {{ risquePluie }}</p>
+    <!-- TODO - A complèter -->
+    <img src="icone" alt="Icone" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Accueil",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      store: false,
+      txt: "MétéoVue",
+      config: null,
+    };
+  },
+  mounted: function () {
+    if (localStorage.getItem("configuration")) {
+      // récup config
+      // TODO - appel API Météo
+    } else {
+      // redirection vers la page de config
+      this.$router.push("/config");
+    }
   },
 };
 </script>
